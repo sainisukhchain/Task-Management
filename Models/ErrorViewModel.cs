6f7b547f2749deafb3a20e42f64f371f61ab59ce
sainisukhchain <sukhchain.saini@outlook.com>
2025-05-15 21:00:24 +0530

    deleted the project from solution

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Task_Management.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
